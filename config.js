f5c989446c1ee90c2845310370cb29f3fa8358a3
Batado <89145165+Batado@users.noreply.github.com>
2021-08-18 21:19:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Ê condi à!',
+    introDesc: ` Làm cc gì mà 
+    Đầu t toàn hình bóng m thế 
+    M thế là deo duoc rui nha`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Phải chăng t đã mê ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng t đã thích ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích t a <33',
+    btnNo: 'Không, Conchos a :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao m lại thích t <3',
+    btnReply: 'Gửi cho m do  <3',
+    reply: 'Thich thì thich  mà không thich thì thich <33333333',
+    mess: 'T biết mà 🥰. Hhehehe  😘😘',
+    messDesc: ' Có gì dẫn m di an nha :))).',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
