58f6de58fda72f052d060520cc7efefbdbb08de5
Dat nguyn <89145165+Batado@users.noreply.github.com>
2021-08-19 19:45:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,17 @@
 const CONFIG = {
-    introTitle: 'Ê condi à!',
-    introDesc: ` Làm cc gì mà 
-    Đầu t toàn hình bóng m thế 
-    M thế là deo duoc rui nha`,
+    introTitle: 'hi ong cha~!',
+    introDesc: ` zợ pít là mí nay chòng bị bịnh nên mịt moi~
+    zữ lém nên hnai zợ sẽ lèm céi nì tặng chòng ó `,
     btnIntro: 'hihi',
-    title: 'Phải chăng t đã mê ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng t đã thích ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích t a <33',
-    btnNo: 'Không, Conchos a :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao m lại thích t <3',
-    btnReply: 'Gửi cho m do  <3',
+    title: 'Phải chăng zợ đã mê ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng zợ đã thích ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là iu ck mãi hui  a <33',
+    btnNo: 'Đố ck nhấn đc chữ ko ó nka a :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao zợ lại iu ck <3',
+    btnReply: 'Gửi cho ck ó  <3',
     reply: 'Thich thì thich  mà không thich thì thich <33333333',
-    mess: 'T biết mà 🥰. Hhehehe  😘😘',
-    messDesc: ' Có gì dẫn m di an nha :))).',
-    btnAccept: 'Okiiiii lun <3',
+    mess: ' biết mà 🥰. Hhehehe  😘😘',
+    messDesc: ' hun chụt chụt   :))).',
+    btnAccept: 'ôki lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
